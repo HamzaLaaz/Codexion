12c52aaeee1cd310f44818448fb3f42843ff1052
HamzaLaaz <laazhamza502@gmail.com>
2026-07-30 06:52:04 +0100

    refactor: centralize parser error handling

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: hlaaz <hlaaz@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 17:19:17 by hlaaz             #+#    #+#             */
-/*   Updated: 2026/07/27 12:52:01 by hlaaz            ###   ########.fr       */
+/*   Updated: 2026/07/30 05:36:43 by hlaaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ static long	ft_atol(const char *str)
 
 	result = 0;
 	i = 0;
+	//todo don't forget handell overflow
 	if (!str || !str[0])
 		return (-1);
 	while (str[i])
@@ -35,25 +36,25 @@ static int	parse_numbers(char **argv, t_config *config)
 {
 	config->nb_coders = ft_atol(argv[1]);
 	if (config->nb_coders == -1)
-		return (printf("Invalid number of coders"));
+		return (error("Invalid number of coders"));
 	config->time_to_burnout = ft_atol(argv[2]);
 	if (config->time_to_burnout == -1)
-		return (printf("Invalid burnout time"));
+		return (error("Invalid burnout time"));
 	config->time_to_compile = ft_atol(argv[3]);
 	if (config->time_to_compile == -1)
-		return (printf("Invalid compile time"));
+		return (error("Invalid compile time"));
 	config->time_to_debug = ft_atol(argv[4]);
 	if (config->time_to_debug == -1)
-		return (printf("Invalid debug time"));
+		return (error("Invalid debug time"));
 	config->time_to_refactor = ft_atol(argv[5]);
 	if (config->time_to_refactor == -1)
-		return (printf("Invalid refactor time"));
+		return (error("Invalid refactor time"));
 	config->nb_required_compiles = ft_atol(argv[6]);
 	if (config->nb_required_compiles == -1)
-		return (printf("Invalid required compiles"));
+		return (error("Invalid required compiles"));
 	config->dongle_cooldown = ft_atol(argv[7]);
 	if (config->dongle_cooldown == -1)
-		return (printf("Invalid cooldown"));
+		return (error("Invalid cooldown"));
 	return (0);
 }
 
@@ -64,33 +65,33 @@ static int	parse_scheduler(char *arg, t_config *config)
 	else if (strcmp(arg, "edf") == 0)
 		config->scheduler = EDF;
 	else
-		return (printf("Invalid scheduler"));
+		return (error("Invalid scheduler"));
 	return (0);
 }
 
 static int	validate_config(t_config *config)
 {
 	if (config->nb_coders <= 0)
-		return (printf("Invalid number of coders"));
+		return (error("Invalid number of coders"));
 	if (config->time_to_burnout <= 0)
-		return (printf("Invalid burnout time"));
+		return (error("Invalid burnout time"));
 	if (config->time_to_compile <= 0)
-		return (printf("Invalid compile time"));
+		return (error("Invalid compile time"));
 	if (config->time_to_debug <= 0)
-		return (printf("Invalid debug time"));
+		return (error("Invalid debug time"));
 	if (config->time_to_refactor <= 0)
-		return (printf("Invalid refactor time"));
+		return (error("Invalid refactor time"));
 	if (config->nb_required_compiles <= 0)
-		return (printf("Invalid required compiles"));
+		return (error("Invalid required compiles"));
 	if (config->dongle_cooldown <= 0)
-		return (printf("Invalid cooldown"));
+		return (error("Invalid cooldown"));
 	return (0);
 }
 
 int	parse_arguments(int argc, char **argv, t_config *config)
 {
 	if (argc != 9)
-		return (printf("Usage: ./codexion "
+		return (error("Usage: ./codexion "
 				"<coders> <burnout> <compile> <debug> "
 				"<refactor> <required_compiles> "
 				"<cooldown> <fifo|edf>"));
