e3250ff13970d3bda4e01ca8845ab612118f46df
HamzaLaaz <laazhamza502@gmail.com>
2026-08-02 18:48:40 +0100

    fix: improve simulation resource cleanup

--- cleanup.c
+++ cleanup.c
@@ -6,30 +6,57 @@
 /*   By: hlaaz <hlaaz@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 00:49:05 by hlaaz             #+#    #+#             */
-/*   Updated: 2026/07/30 06:07:51 by hlaaz            ###   ########.fr       */
+/*   Updated: 2026/08/02 18:48:15 by hlaaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-static void	destroy_dongles(t_simulation *sim, int nb_dongles_init)
+static void	destroy_dongles(t_simulation *sim, int nb_dongles)
 {
 	int	i;
 
 	i = 0;
-	while (i < nb_dongles_init)
+	while (i < nb_dongles)
 	{
-		pthread_mutex_destroy(&sim->dongles[i].mutex);
 		pthread_cond_destroy(&sim->dongles[i].cond);
+		pthread_mutex_destroy(&sim->dongles[i].mutex);
 		i++;
 	}
 }
 
-void	cleanup_simulation(t_simulation *sim, int nb_dongles_init)
+static void	destroy_coders(t_simulation *sim, int nb_coders)
 {
-	destroy_dongles(sim, nb_dongles_init);
-	free(sim->dongles);
-	free(sim->coders);
+	int	i;
+
+	i = 0;
+	while (i < nb_coders)
+	{
+		pthread_cond_destroy(&sim->coders[i].cond);
+		i++;
+	}
+}
+
+void	cleanup_simulation(t_simulation *sim, int nb_dongles, int nb_coders)
+{
+	if (sim->coders)
+	{
+		destroy_coders(sim, nb_coders);
+		free(sim->coders);
+		sim->coders = NULL;
+	}
+	if (sim->dongles)
+	{
+		destroy_dongles(sim, nb_dongles);
+		free(sim->dongles);
+		sim->dongles = NULL;
+	}
+	pthread_mutex_destroy(&sim->log_mutex);
+	pthread_mutex_destroy(&sim->state_mutex);
+}
+
+void	destroy_mutex(t_simulation *sim)
+{
 	pthread_mutex_destroy(&sim->log_mutex);
 	pthread_mutex_destroy(&sim->state_mutex);
 }
@@ -38,10 +65,4 @@ int	error(char *msg)
 {
 	printf("%s\n", msg);
 	return (1);
-}
-
-void destroy_mutex(t_simulation *sim)
-{
-	pthread_mutex_destroy(&sim->state_mutex);
-	pthread_mutex_destroy(&sim->log_mutex);
 }
