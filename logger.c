87caa8fde9bac207433cee9df716824d7bc34522
HamzaLaaz <laazhamza502@gmail.com>
2026-08-04 23:20:40 +0100

    refactor: simplify argument parsing and validation

--- logger.c
+++ logger.c
@@ -6,7 +6,7 @@
 /*   By: hlaaz <hlaaz@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/30 00:49:12 by hlaaz             #+#    #+#             */
-/*   Updated: 2026/07/31 06:42:24 by hlaaz            ###   ########.fr       */
+/*   Updated: 2026/08/01 04:33:45 by hlaaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	log_action(t_coder *coder, char *msg)
 {
+	long	time;
+
 	pthread_mutex_lock(&coder->sim->log_mutex);
-	printf("Coder %d %s\n", coder->id, msg);
+	time = current_time_ms() - coder->sim->start_time;
+	printf("%ld %d %s\n", time, coder->id, msg);
 	pthread_mutex_unlock(&coder->sim->log_mutex);
 }
--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: hlaaz <hlaaz@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 17:19:17 by hlaaz             #+#    #+#             */
-/*   Updated: 2026/07/30 05:36:43 by hlaaz            ###   ########.fr       */
+/*   Updated: 2026/08/04 22:35:49 by hlaaz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,89 +17,80 @@ static long	ft_atol(const char *str)
 	long	result;
 	int		i;
 
+	if (!str || !*str || (str[0] == '0' && str[1]))
+		return (-1);
 	result = 0;
 	i = 0;
-	//todo don't forget handell overflow
-	if (!str || !str[0])
-		return (-1);
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
 			return (-1);
-		result = result * 10 + (str[i] - '0');
-		i++;
+		if (result > (LONG_MAX - (str[i] - '0')) / 10)
+			return (-1);
+		result = result * 10 + (str[i++] - '0');
 	}
 	return (result);
 }
 
-static int	parse_numbers(char **argv, t_config *config)
+static int	parse_numbers(char **av, t_config *cfg)
 {
-	config->nb_coders = ft_atol(argv[1]);
-	if (config->nb_coders == -1)
+	long	v[7];
+	int		i;
+
+	i = 0;
+	while (i < 7)
+	{
+		v[i] = ft_atol(av[i + 1]);
+		if (v[i] < 0 || v[i] > INT_MAX)
+			return (error("Invalid numeric argument"));
+		i++;
+	}
+	cfg->nb_coders = (int)v[0];
+	cfg->time_to_burnout = v[1];
+	cfg->time_to_compile = v[2];
+	cfg->time_to_debug = v[3];
+	cfg->time_to_refactor = v[4];
+	cfg->nb_required_compiles = (int)v[5];
+	cfg->dongle_cooldown = v[6];
+	return (0);
+}
+
+static int	validate_config(t_config *cfg)
+{
+	if (cfg->nb_coders <= 0 || cfg->nb_coders > 1000)
 		return (error("Invalid number of coders"));
-	config->time_to_burnout = ft_atol(argv[2]);
-	if (config->time_to_burnout == -1)
-		return (error("Invalid burnout time"));
-	config->time_to_compile = ft_atol(argv[3]);
-	if (config->time_to_compile == -1)
-		return (error("Invalid compile time"));
-	config->time_to_debug = ft_atol(argv[4]);
-	if (config->time_to_debug == -1)
-		return (error("Invalid debug time"));
-	config->time_to_refactor = ft_atol(argv[5]);
-	if (config->time_to_refactor == -1)
-		return (error("Invalid refactor time"));
-	config->nb_required_compiles = ft_atol(argv[6]);
-	if (config->nb_required_compiles == -1)
+	if (cfg->time_to_burnout <= 0 || cfg->time_to_compile <= 0)
+		return (error("Invalid timing"));
+	if (cfg->time_to_debug <= 0 || cfg->time_to_refactor <= 0)
+		return (error("Invalid timing"));
+	if (cfg->nb_required_compiles <= 0)
 		return (error("Invalid required compiles"));
-	config->dongle_cooldown = ft_atol(argv[7]);
-	if (config->dongle_cooldown == -1)
+	if (cfg->dongle_cooldown < 0)
 		return (error("Invalid cooldown"));
 	return (0);
 }
 
-static int	parse_scheduler(char *arg, t_config *config)
+static int	parse_scheduler(char *arg, t_config *cfg)
 {
-	if (strcmp(arg, "fifo") == 0)
-		config->scheduler = FIFO;
-	else if (strcmp(arg, "edf") == 0)
-		config->scheduler = EDF;
+	if (!strcmp(arg, "fifo"))
+		cfg->scheduler = FIFO;
+	else if (!strcmp(arg, "edf"))
+		cfg->scheduler = EDF;
 	else
 		return (error("Invalid scheduler"));
 	return (0);
 }
 
-static int	validate_config(t_config *config)
+int	parse_arguments(int ac, char **av, t_config *cfg)
 {
-	if (config->nb_coders <= 0)
-		return (error("Invalid number of coders"));
-	if (config->time_to_burnout <= 0)
-		return (error("Invalid burnout time"));
-	if (config->time_to_compile <= 0)
-		return (error("Invalid compile time"));
-	if (config->time_to_debug <= 0)
-		return (error("Invalid debug time"));
-	if (config->time_to_refactor <= 0)
-		return (error("Invalid refactor time"));
-	if (config->nb_required_compiles <= 0)
-		return (error("Invalid required compiles"));
-	if (config->dongle_cooldown <= 0)
-		return (error("Invalid cooldown"));
-	return (0);
-}
-
-int	parse_arguments(int argc, char **argv, t_config *config)
-{
-	if (argc != 9)
-		return (error("Usage: ./codexion "
-				"<coders> <burnout> <compile> <debug> "
-				"<refactor> <required_compiles> "
+	*cfg = (t_config){0};
+	if (ac != 9)
+		return (error("Usage: ./codexion <coders> <burnout> <compile> "
+				"<debug> <refactor> <required_compiles> "
 				"<cooldown> <fifo|edf>"));
-	if (parse_numbers(argv, config))
+	if (parse_numbers(av, cfg))
 		return (1);
-	if (parse_scheduler(argv[8], config))
+	if (parse_scheduler(av[8], cfg))
 		return (1);
-	if (validate_config(config))
-		return (1);
-	return (0);
+	return (validate_config(cfg));
 }
